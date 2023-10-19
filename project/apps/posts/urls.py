0e55bf8dd0dbe2308843d6e75b8d5be6c08de9d7
khayratdinov <khayratdinov.np@gmail.com>
2023-10-19 11:47:59 +0500

    feat: 👍 like system added

--- project/apps/posts/urls.py
+++ project/apps/posts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from project.apps.posts.views import (
     PostsListView,
     PostDetailView,
+    LikeView,
 )
 
 urlpatterns = [
     path("", PostsListView.as_view(), name="index"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("like/<int:pk>/", LikeView.as_view(), name="like_post"),
 ]
--- project/apps/posts/views.py
+++ project/apps/posts/views.py
@@ -7,10 +7,14 @@ from django.views import View
 
 # ============================================================================ #
 
-from project.apps.posts.forms import CommentForm
+
 from project.apps.posts.models import Post, PostComment
 
 # ============================================================================ #
+
+
+def home(request):
+    return render(request, "home.html")
 
 
 # =============================== POSTSLISTVIEW ============================== #
@@ -19,7 +23,7 @@ from project.apps.posts.models import Post, PostComment
 class PostsListView(View):
     def get(self, request):
         posts = Post.objects.filter(status=True).order_by("-created_at")
-        paginator = Paginator(posts, 10)
+        paginator = Paginator(posts, 10)  # 10 posts per page
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
         return render(request, "index.html", {"page_obj": page_obj})
@@ -32,3 +36,19 @@ class PostDetailView(View):
     def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         return render(request, "post_detail.html", {"post": post})
+
+
+# ================================= LIKEVIEW ================================= #
+
+
+class LikeView(View):
+    @method_decorator(login_required)
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        if request.user in post.likes.all():
+            post.likes.remove(request.user)
+            liked = False
+        else:
+            post.likes.add(request.user)
+            liked = True
+        return JsonResponse({"liked": liked, "total_likes": post.total_likes})
